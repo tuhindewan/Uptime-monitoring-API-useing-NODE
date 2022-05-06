413d11cefbaa86fd3efc1c6af16d329a5f981f66
tuhindewan <tuhinsshadow@gmail.com>
2022-05-06 22:56:18 +0600

    code refactor on index.js script

--- index.js
+++ index.js
@@ -7,8 +7,7 @@
 
 // dependencies
 const http = require('http');
-const url = require('url');
-const { StringDecoder } = require('string_decoder');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
 // app object - module scaffolding
 const app = {};
@@ -27,31 +26,7 @@ app.createServer = () => {
 };
 
 // handle request response
-app.handleReqRes = (req, res) => {
-    // request handle
-    // get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
-    });
-
-    req.on('end', () => {
-        realData += decoder.end();
-
-        console.log(realData);
-        // response handle
-        res.end('Hello world');
-    });
-};
+app.handleReqRes = handleReqRes;
 
 // start the server
 app.createServer();
