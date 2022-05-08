2c39f9ca880822d78209f14d1d54d0c3a59e9bd0
tuhindewan <tuhinsshadow@gmail.com>
2022-05-08 20:06:54 +0600

    setup environment variable for server starting

---
+++ helpers/environments.js
@@ -0,0 +1,31 @@
+/**
+ * Title: Environment variables
+ * Description: Handle application environment variables
+ * Author: Saiduzzaman Tohin
+ * Date: 06/05/2022
+ */
+
+// module scaffolding
+const environments = {};
+
+environments.staging = {
+    port: 3000,
+    envName: 'staging',
+};
+
+environments.production = {
+    port: 5000,
+    envName: 'production',
+};
+
+// determine which environment was passed
+const currentEnvironment =
+    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+
+// export corresponding environment object
+const environmentToExport =
+    typeof environments[currentEnvironment] === 'object'
+        ? environments[currentEnvironment]
+        : environments.staging;
+
+module.exports = environmentToExport;
